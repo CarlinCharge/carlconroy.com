340ae67cdd65501a5f6f5b17d203e8844bfba53a
CarlinCharge <sprintman210@gmail.com>
2021-03-22 12:40:01 -0400

    fixes default link styles

--- src/components/Social/styles.js
+++ src/components/Social/styles.js
@@ -2,6 +2,9 @@ import * as Colors from "../../styles/Colors";
 import styled from "styled-components";
 
 export const SocialLink = styled.a`
+  &:link {
+    color: ${Colors.HomepageText};
+  }
   &:visited {
     color: ${Colors.HomepageText};
   }
