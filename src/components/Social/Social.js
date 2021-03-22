cbab80f56dadfb87eb8b25ceabaf66a903eb4e78
CarlinCharge <sprintman210@gmail.com>
2021-03-22 11:48:47 -0400

    adds twitter boxicon

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Github } from "@styled-icons/boxicons-logos/Github";
 import { Instagram } from "@styled-icons/boxicons-logos/Instagram";
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter";
 
 const LogoGithub = styled(Github)`
   width: 77px;
@@ -15,10 +16,17 @@ const LogoInstagram = styled(Instagram)`
   height: 80px;
   float: right;
 `;
+
+const LogoTwitter = styled(Twitter)`
+  width: 77px;
+  height: 80px;
+  flex-direction: column;
+`;
 const Social = () => (
   <div>
     <LogoGithub href="https://github.com/CarlinCharge" />
     <LogoInstagram href="https://instagram.com/CarlConroydesign" />
+    <LogoTwitter href="https://twitter.com/CarlWearsPrada" />
   </div>
 );
 
