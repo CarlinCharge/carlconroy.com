fa31f7950ee718e9295d1503d34a524a07d6b9f9
CarlinCharge <sprintman210@gmail.com>
2021-03-26 17:32:14 -0400

    homepage fixes

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -7,8 +7,7 @@ const Homepage = () => {
   const theme = "user";
   return (
     <S.Index>
-      <S.Title theme={theme}>Projects</S.Title>
-
+      <S.Title>The Future Location of My Digital Greenhouse 🌱</S.Title>
       <S.MainText href="https://carlconroydesign.com">
         Carl Conroy Design
       </S.MainText>
