a4d053b7710f0e0eb21c1536d8d935ad557d84fd
CarlinCharge <sprintman210@gmail.com>
2021-03-26 17:06:24 -0400

    adds main font to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,22 @@ module.exports = {
     "gatsby-plugin-mdx",
     { resolve: `gatsby-plugin-styled-components` },
     {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+          },
+        ],
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
         path: "./src/pages/",
       },
+
       __key: "pages",
     },
   ],
